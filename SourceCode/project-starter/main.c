4a280dc57d0af8610f0b40ea18272d6ef8247b30
Sangjune Lee <csjune411@gmail.com>
2023-07-07 17:29:48 -0400

    Route additions
    excluded Route has been added(class instruction)

--- SourceCode/project-starter/main.c
+++ SourceCode/project-starter/main.c
@@ -6,8 +6,12 @@ int main(void)
 {
 	struct Map baseMap = populateMap();
 	struct Route blueRoute = getBlueRoute();
+	struct Route greenRoute = getGreenRoute();
+	struct Route yellowRoute = getYellowRoute();
 	struct Map routeMap = addRoute(&baseMap, &blueRoute);
-
+	routeMap = addRoute(&routeMap, &greenRoute);
+	routeMap = addRoute(&routeMap, &yellowRoute);
+	routeMap = addRoute(&routeMap, &blueRoute); 
 	printMap(&routeMap, 1, 1);
 
 	return 0;
